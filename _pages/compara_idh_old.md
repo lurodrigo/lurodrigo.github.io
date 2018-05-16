e8b410d459005bb186968d756e606f32d609dc1c
Luiz Rodrigo de Souza <me@lurodrigo.com>
2018-05-16 20:04:53 -0300

    idh em javascript

--- _pages/compara_idh_old.md
+++ _pages/compara_idh_old.md
@@ -1,24 +1,12 @@
 ---
 title: "Compara IDH"
 layout: single
-excerpt: "Escolha um bairro e eu te digo os países com desenvolvimento humano similar."
+excerpt: ""
 sitemap: false
-permalink: /compara_idh/
-script: |
-    <script src="/assets/js/selectize.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="/assets/js/idh.js"></script>
+permalink: /compara_idh_old/
 ---
 
-<link rel="stylesheet" type="text/css" href="{{site.url}}/assets/css/selectize.default.css">
-
-<style>
-  #municipio {
-    width: 700px;
-  }
-</style>
-	
-<p><select id="municipio"></select></p>
-<p id="texto"></p>
+<iframe src="https://lurodrigo.shinyapps.io/compara_idh/" width="1000px" height="300px" style="border:0;"></iframe>
 
 Observações: Os dados de IDH dos bairros foram obtidos [aqui][1]; os dos países, [aqui][2]. Só tenho dados de IDH de bairros (às vezes agregações maiores) de algumas regiões metropolitanas. Os dados são de 2010. A comparação entre bairros e países pode não ser muito justa (considere que países são muito mais heterogêneos que bairros). Leve isso na esportiva.
 
---
+++ assets/js/idh.js
@@ -0,0 +1,49 @@
+var tb_idhm = [];
+var tb_idh = [];
+
+set_message = function(id) {
+    linha = tb_idhm[id-1];
+    pais1 = tb_idh[linha.p1-1];
+    pais2 = tb_idh[linha.p2-1];
+    pais3 = tb_idh[linha.p3-1];
+    pais4 = tb_idh[linha.p4-1];
+
+    $('#texto').html(
+      `<strong>${linha.Nome}</strong> tem IDH ${linha.IDHM}. <strong>${pais1.Pais}</strong>,
+    com ${pais1.IDH}, é o país com IDH mais próximo. Outros países com nível
+    de desenvolvimento humano similar são
+     <strong>${pais2.Pais}</strong> (${pais2.IDH}), <strong>${pais3.Pais}</strong> (${pais3.IDH}) e <strong>${pais4.Pais}</strong> (${pais4.IDH}).`
+    );
+}
+
+window.jQuery('#texto').html("aaaa");
+
+$(document).ready(function() {
+    $('#municipio').selectize({
+        options: [],
+        labelField: 'label',
+        valueField: 'id',
+        placeholder: "Escolha um município",
+        searchField: ['label'],
+        onChange: function(value) {
+          set_message(value);
+        }
+    });
+
+    $.getJSON("http://lurodrigo.com/data/tb_idhm_precomp.json", function(data) {
+        tb_idhm = data;
+        //inicializa opções
+        selectize = $('#municipio')[0].selectize;
+        selectize.clearOptions();
+
+        for (i = 0; i < tb_idhm.length; i++) {
+            selectize.addOption(tb_idhm[i]);
+        }
+
+        selectize.refreshOptions();
+    });
+
+    $.getJSON("http://lurodrigo.com/data/tb_idh_min.json", function(data) {
+      tb_idh = data;
+    });
+});
